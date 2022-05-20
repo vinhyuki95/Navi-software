c9637a379369500bbd0d09a335cf78546988a7ef
Vinh Le <vinhyuki1995@gmail.com>
2022-05-20 15:32:10 +0700

    slick-slider__box-slider

---
+++ libs/app.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+  $('.box-slider').slick({
+     slidesToShow:5,
+     slidesToScroll: 1,
+     arrows: true,
+     prevArrow:"<button type='button' class='slick-prev slick-slider'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+     nextArrow:"<button type='button' class='slick-next slick-slider'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+  });
+});
+	
