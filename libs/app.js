c02ef9777a47776ef23a66da97fd1427d45438f6
Vinh Le <vinhyuki1995@gmail.com>
2022-05-24 10:28:25 +0700

    build-market2

--- libs/app.js
+++ libs/app.js
@@ -1,11 +1,22 @@
 
 $(document).ready(function(){
+  // slick-slider
   $('.box-slider').slick({
      slidesToShow:5,
-     slidesToScroll: 1,
-     arrows: true,
-     prevArrow:"<button type='button' class='slick-prev slick-slider'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
-     nextArrow:"<button type='button' class='slick-next slick-slider'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+     slidesToScroll:2,
+     arrows: false,
+    //  prevArrow:"<button type='button' class='slick-prev slick-slider'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    //  nextArrow:"<button type='button' class='slick-next slick-slider'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    responsive:[
+      {
+        breakpoint:1800,
+        settings:{
+          slidesToShow:3,
+          slidesToScroll:1,
+        }
+      }, 
+    ]
   });
+  // click vào icon thembnail
+
 });
-	
